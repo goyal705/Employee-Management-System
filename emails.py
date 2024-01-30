7dcb6a71f25ce0016a85675b5aaad51b40ea32f7
goyal705 <75295353+goyal705@users.noreply.github.com>
2024-01-31 00:04:59 +0530

    Update emails.py

--- emails.py
+++ emails.py
@@ -2,6 +2,7 @@ import smtplib
 import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import pymongo
 import time
 from datetime import datetime
 import os
@@ -9,11 +10,32 @@ import os
 usernamedb=os.getenv('usernamedb')
 password=os.getenv('password')
 
-def sendmailprod(data):
+def extractdata():
+    connection_url = f"mongodb+srv://{usernamedb}:{password}@cluster0.znfsndq.mongodb.net/?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(connection_url)
+    db = client["EmployeeManagementSystem"]
+    collection = db["Emails"]
+    response=collection.find_one({})
+    client.close()
+    return response["data"]
+
+def removedata(data):
+    connection_url = f"mongodb+srv://{usernamedb}:{password}@cluster0.znfsndq.mongodb.net/?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(connection_url)
+    db = client["EmployeeManagementSystem"]
+    collection = db["Emails"]
+    update_query = {"$pull":{"data": data}}
+    response=collection.update_one({},update_query)
+    print("Removed email data:",data["recipientmail"])
+    client.close()
+
+def sendmail():
+    data=extractdata()
+    for i in data:
         start=time.time()
-        recipient=data["recipientmail"]
-        subject=data["subject"]
-        body=data["body"]
+        recipient=i["recipientmail"]
+        subject=i["subject"]
+        body=i["body"]
         
         email = "tushargoyal628@outlook.com"
         password = os.getenv('outlook')
@@ -38,6 +60,13 @@ def sendmailprod(data):
         current_time = datetime.now()
         time_string = current_time.strftime("%Y-%m-%d %H:%M:%S")
         print(f"Email sent successfully to {recipient} at {time_string}")
-        # removedata(i)
+        removedata(i)
         end=time.time()
         print("Time taken: ",end-start)
+    
+
+while True:
+    try:
+        sendmail()
+    except Exception as err:
+        print(err)
